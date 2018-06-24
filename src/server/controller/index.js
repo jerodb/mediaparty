fcfd890c78c0026a849f46e7b1d5b40317b4e65b
jerodb <jero.db@live.com>
2018-06-24 18:55:21 -0300

    correction

--- src/server/controller/index.js
+++ src/server/controller/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import acceptLang from 'accept-language';
+// import acceptLang from 'accept-language';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Router from '../../components/Router';
 
 
 const all = (req, res) => {
-  const lang = req.header('Accept-Language');
+  let lang = req.header('Accept-Language');
   console.log(lang);
+  lang = lang.split('-');
+  // const parsed = acceptLang.parse(lang);
+  const locale = ['es', 'en'].indexOf(lang) !== -1 ? lang : 'en'
 
-  const parsed = acceptLang.parse(req.header('Accept-Language'));
-  const locale = ['es', 'en'].indexOf(parsed[0]) !== -1 ? parsed[0] : 'en'
-
-  console.log(parsed)
-  console.log(locale)
+  console.log(lang);
+  // console.log(parsed);
+  console.log(locale);
 
   // locale = 'en';
 
