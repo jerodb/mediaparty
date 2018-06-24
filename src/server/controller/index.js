4f1776ed57b4d8297a2a5e873698793606582334
jerodb <jero.db@live.com>
2018-06-24 19:40:25 -0300

    correction

--- src/server/controller/index.js
+++ src/server/controller/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import acceptLang from 'accept-language';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import Router from '../../components/Router';
@@ -8,16 +7,12 @@ require('dotenv').config();
 
 const all = (req, res) => {
   let lang = req.header('Accept-Language');
-  console.log(lang);
-  const parsed = acceptLang.parse(lang);
-  lang = lang ? lang.split('-') : ['es'];
-  const locale = ['es', 'en'].indexOf(lang[0]) !== -1 ? lang[0] : process.env.DEFAULT_LANG
+  let locale = process.env.DEFAULT_LANG;
 
-  console.log(lang);
-  console.log(parsed);
-  console.log(locale);
-
-  // locale = 'en';
+  if (lang) {
+    lang = lang.split('-')[0];
+    locale = ['es', 'en'].indexOf(lang) !== -1 ? lang : locale;
+  }
 
   // generate the React markup for the current route
   const markup = renderToString(<StaticRouter context={ {} } location={ req.url }><Router locale={ locale } /></StaticRouter>);
