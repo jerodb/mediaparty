16a9c922f740f77d077a36b15bd847cd00c10b1e
jerodb <jero.db@live.com>
2019-07-01 15:14:59 -0300

    Marian changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -132,11 +132,11 @@ class Home extends React.Component {
           <div className="col-sm-12 col-xs-12 event__forms">
             <p>{ proposal }</p>
             <div className="event__forms_links">
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSd2qysTQB0GCYZALBMnf9lPttA8yyl-tUvq0VbpzYSZYrM8gw/viewform" onClick={ () => { handleGAClick(gaEvents.hero[9]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ applyHere }</a>
-              { /* <a href="https://goo.gl/forms/gbo24YkCZozAQDQe2" onClick={ () => { handleGAClick(gaEvents.hero[3]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ workshop }</a> /
-              <a href="https://goo.gl/forms/N84m60yy8XjhsYay1" onClick={ () => { handleGAClick(gaEvents.hero[4]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ talk }</a> /
-              <a href="https://goo.gl/forms/tx8Dc7DOHZDJTiyC2" onClick={ () => { handleGAClick(gaEvents.hero[5]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ fair }</a> /
-              <a href="https://goo.gl/forms/LTeksx8BGWIGOSSY2" onClick={ () => { handleGAClick(gaEvents.hero[8]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ award }</a> */ }
+              { /* <a href="https://docs.google.com/forms/d/e/1FAIpQLSd2qysTQB0GCYZALBMnf9lPttA8yyl-tUvq0VbpzYSZYrM8gw/viewform" onClick={ () => { handleGAClick(gaEvents.hero[9]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ applyHere }</a> */ }
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSdMpxrwWyUYYRzG1mliRBaX342I8ar6I5PuueTKQaSPGNntBQ/viewform?usp=sf_link" onClick={ () => { handleGAClick(gaEvents.hero[3]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ workshop }</a> /
+              { /* <a href="https://goo.gl/forms/N84m60yy8XjhsYay1" onClick={ () => { handleGAClick(gaEvents.hero[4]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ talk }</a> / */ }
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSehECCx60bQxRPkRPUyeJTpjY_cuPQQp9LvADc2LzlrWTzNZQ/viewform?usp=sf_link" onClick={ () => { handleGAClick(gaEvents.hero[5]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ fair }</a>
+              { /* <a href="https://goo.gl/forms/LTeksx8BGWIGOSSY2" onClick={ () => { handleGAClick(gaEvents.hero[8]); } } target="_blank" rel="noopener noreferrer" className="event__form_cta">{ award }</a> */ }
               { /*
               <Link to={ '/posit-workshop' } className="event__form_cta" onClick={ () => { handleGAClick(gaEvents.hero[3]); } }>{ workshop }</Link> /
               <Link to={ '/posit-lightning-talk' } className="event__form_cta" onClick={ () => { handleGAClick(gaEvents.hero[4]); } }>{ talk }</Link> /
