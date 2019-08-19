ccd0a380bdc8ac0f49671aef0c3f909d8f91210c
jerodb <jero.db@live.com>
2019-08-19 13:13:04 -0300

    New Venue component

---
+++ src/components/Venue/VenueComponent.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Venue = ({
+  allowMapEvents, mapIframe, mapEvents, venue
+}) => (
+  <section id="venue" className="venue">
+    <div className="event__venue_info">
+      <h2>{ venue }</h2>
+      <p className="event__venue_name">Ciudad Cultural Konex</p>
+      <p className="event__venue_address">Sarmiento 3131, Buenos Aires, Argentina</p>
+    </div>
+    <button
+      className={`${mapEvents} map-wrapper interactive-element`}
+      dangerouslySetInnerHTML={mapIframe}
+      onClick={() => allowMapEvents(true)}
+      onMouseLeave={() => allowMapEvents(false)}
+      type="button"
+    />
+  </section>
+)
+
+Venue.propTypes = {
+  allowMapEvents: PropTypes.func,
+  mapIframe: PropTypes.object,
+  mapEvents: PropTypes.string,
+  venue: PropTypes.string,
+}
+
+export default Venue
