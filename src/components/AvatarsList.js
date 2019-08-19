fc4db84f07226dfb233b090245539ea4eddb8fa9
jerodb <jero.db@live.com>
2019-08-19 13:08:51 -0300

    remove Image handling from AvatarsList

--- src/components/AvatarsList.js
+++ src/components/AvatarsList.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { phSpeakers } from '../res/images'
 
 const AvatarsList = ({
   Avatar, list, name, type
-}) => (
-  <section className={`event__${type} wrapper`}>
-    <h2>
-      <div className="title-txt">
-        {name}
-      </div>
-      <div className="title-line" />
-    </h2>
-    <div className="row flex-wrapper">
-      {
-      list.map((data, k) => {
-        let pic = phSpeakers
-        let url = data && data.url
+}) => {
+  if (list.length > 0) {
+    return (
+      <section className={`event__${type} wrapper`}>
+        <h2>
+          <div className="title-txt">
+            {name}
+          </div>
+          <div className="title-line" />
+        </h2>
+        <div className="row flex-wrapper">
+          {
+        list.map((data, k) => {
+          const url = data && data.url
+          const urlLink = url.startsWith('http') ? url : `http://${url}`
 
-        if (data && data.avatar && !data.avatar.startsWith('https://graph.facebook')) {
-          pic = data.avatar
-        }
+          return (
+            <Avatar
+              {...data}
+              key={JSON.stringify(k)}
+              imageSrc={data.avatar}
+              urlLink={urlLink}
+            />
+          )
+        })
+      }
+        </div>
+      </section>
+    )
+  }
 
-        if (url && url.startsWith('http')) {
-          url = `http://${url}`
-        }
-
-        return <Avatar key={JSON.stringify(k)} {...data} pic={pic} url={url} />
-      })
-    }
-    </div>
-  </section>
-)
+  return <></>
+}
 
 AvatarsList.propTypes = {
   Avatar: PropTypes.func,
