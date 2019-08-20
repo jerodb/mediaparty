ec825df2a6d95e6f53c7da6aa051a5c47b4145f0
jerodb <jero.db@live.com>
2019-08-20 19:11:44 -0300

    load sched script after component loaded

--- src/screens/Sched.js
+++ src/screens/Sched.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const Sched = () => (
-  <>
-    <a
-      id="sched-embed"
-      href="https://mediaparty2019.sched.com/"
-      data-sched-sidebar="no"
-    >
-    View the Media Party 2019 schedule &amp; directory.
-    </a>
-  </>
-)
+const Sched = () => {
+  let sched = ''
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://mediaparty2019.sched.com/js/embed.js'
+    script.async = true
+    sched.appendChild(script)
+  })
+
+  return (
+    <>
+      <a
+        id="sched-embed"
+        data-sched-sidebar="no"
+        href="https://mediaparty2019.sched.com/"
+      >
+        View the Media Party 2019 schedule.
+      </a>
+
+      <div ref={ref => { sched = ref }} />
+    </>
+  )
+}
 
 export default Sched
