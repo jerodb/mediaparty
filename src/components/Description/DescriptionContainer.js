a8c11d1e00ca59d6be2765ddbb7b26336af4aa57
jerodb <jero.db@live.com>
2019-08-20 15:21:13 -0300

    remove useEffect

--- src/components/Description/DescriptionContainer.js
+++ src/components/Description/DescriptionContainer.js
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Description from './DescriptionComponent'
 
 const DescriptionContainer = ({ description }) => {
-  const [videoIframe, setVideoIframe] = useState({ __html: '' })
-
-  useEffect(() => {
-    setVideoIframe({ __html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/-STplo5GrxA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' })
-  }, [videoIframe])
+  const videoIframe = { __html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/-STplo5GrxA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' }
 
   return <Description description={description} video={videoIframe} />
 }
@@ -15,4 +11,5 @@ const DescriptionContainer = ({ description }) => {
 DescriptionContainer.propTypes = {
   description: PropTypes.object,
 }
+
 export default DescriptionContainer
--- src/components/Venue/VenueContainer.js
+++ src/components/Venue/VenueContainer.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Venue from './VenueComponent'
 
 const VenueContainer = ({ venue }) => {
   const [mapEvents, setMapEvents] = useState('notAllowEvents')
-  const [mapIframe, setMapIframe] = useState({ __html: '' })
 
-  useEffect(() => {
-    setMapIframe({ __html: '<iframe name="iframe-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13135.614888414615!2d-58.41068!3d-34.606596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6dd823092cb367d9!2sCiudad+Cultural+Konex!5e0!3m2!1sen!2sar!4v1498682731427" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen></iframe>' })
-  })
+  const mapIframe = { __html: '<iframe name="iframe-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13135.614888414615!2d-58.41068!3d-34.606596!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6dd823092cb367d9!2sCiudad+Cultural+Konex!5e0!3m2!1sen!2sar!4v1498682731427" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen></iframe>' }
 
   const allowMapEvents = allow => {
     const state = allow ? 'allowEvents' : 'notAllowEvents'
