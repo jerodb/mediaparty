9098a8b906847de0e9f5e0469d7e8080eaea9594
jerodb <jero.db@live.com>
2019-08-21 06:48:33 -0300

    add GA pageview

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import ReactGA from 'react-ga'
 
-const NotFound = ({ back, msg, root }) => (
-  <div className="not-found">
-    <h1>404</h1>
-    <h2>{ msg }</h2>
-    <p>
-      <Link to={root}>{ back }</Link>
-    </p>
-  </div>
-)
+const NotFound = ({ back, msg, root }) => {
+  useEffect(() => {
+    ReactGA.pageview(`${window.location.pathname}${window.location.search}not-found`)
+  }, [])
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>{ msg }</h2>
+      <p>
+        <Link to={root}>{ back }</Link>
+      </p>
+    </div>
+  )
+}
 
 NotFound.propTypes = {
   back: PropTypes.string,
