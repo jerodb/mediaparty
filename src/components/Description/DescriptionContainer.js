0b322eeee3d1500ee90dc6bb6ab35de0b3fec281
jerodb <jero.db@live.com>
2019-08-27 00:47:34 -0300

    update description video

--- src/components/Description/DescriptionContainer.js
+++ src/components/Description/DescriptionContainer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Description from './DescriptionComponent'
 
 const DescriptionContainer = ({ description }) => {
-  const videoIframe = { __html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/-STplo5GrxA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' }
+  const videoIframe = { __html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oUz5IPkV3kA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' }
 
   return <Description description={description} video={videoIframe} />
 }
