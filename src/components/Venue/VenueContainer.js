9f73ddde13dc9f2ff474e8f8bb997cf46ba2115a
jerodb <jero.db@live.com>
2019-08-27 19:49:31 -0300

    set map width to 100%

--- src/components/Venue/VenueContainer.js
+++ src/components/Venue/VenueContainer.js
@@ -5,7 +5,7 @@ import Venue from './VenueComponent'
 const VenueContainer = ({ venue }) => {
   const [mapEvents, setMapEvents] = useState('notAllowEvents')
 
-  const mapIframe = { __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26271.24433072853!2d-58.410707!3d-34.60655!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6dd823092cb367d9!2sCiudad%20Cultural%20Konex!5e0!3m2!1sen!2sar!4v1566945959120!5m2!1sen!2sar" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' }
+  const mapIframe = { __html: '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26271.24433072853!2d-58.410707!3d-34.60655!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6dd823092cb367d9!2sCiudad%20Cultural%20Konex!5e0!3m2!1sen!2sar!4v1566945959120!5m2!1sen!2sar" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>' }
 
   const allowMapEvents = allow => {
     const state = allow ? 'allowEvents' : 'notAllowEvents'
